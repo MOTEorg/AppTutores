9c3ee5ee013a77f964599e9e5c49a82c9fd3fb41
FelipeTorr <felipto@hotmail.es>
2016-11-21 00:49:59 -0500

    funcionando envios web
    -Funciona envio http y recoleccion de respuesta
    -Se pasa a la siguiente pantalla

--- app/src/main/java/com/motetronica/apptutores/MainActivity.java
+++ app/src/main/java/com/motetronica/apptutores/MainActivity.java
@@ -1,5 +1,7 @@
 package com.motetronica.apptutores;
 
+import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,13 +9,30 @@ import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    GeneradorConsultas consultas = new GeneradorConsultas();
+
     EditText usuario;
     EditText contrasena;
+    String respuesta;
+    public android.os.Handler h_consultas=new android.os.Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            setRespuesta((String) msg.obj);
+            Log.d("Response",respuesta);
+            if (respuesta!=null){
+            if (!respuesta.equals("0")){
+            Intent i=new Intent(MainActivity.this, Busqueda.class);
+            i.putExtra("Cedula",respuesta);
+            startActivity(i);
+        }
+        }
+        }
+    };
 
-    @Override
+    GeneradorConsultas consultas = new GeneradorConsultas(this.h_consultas);
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
@@ -29,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     void ingresar(View v){
         //consulta
+
         usuario = (EditText) findViewById(R.id.txtuser);
         contrasena = (EditText) findViewById(R.id.txtpass);
         ArrayList<String> claves = new ArrayList<String>();
@@ -40,12 +60,21 @@ public class MainActivity extends AppCompatActivity {
         String consult = consultas.formarPares(claves, valores);
         try {
             consultas.sendConsultaPost("consultar_existencia.php",consult);
-            String resp = consultas.respuesta;
-            Log.d("Respuesta",resp);
+            //Lo que se hace luego se encuentra en el Handler en HandleMessage
         } catch (Exception e) {
             e.printStackTrace();
             Log.d("envio fallido","error_envio");
         }
 
+    }
+    void ir_registrar(View v){
+        Log.d("Respuesta",respuesta);
+    }
+    public void setRespuesta(String resp){
+        respuesta=resp;
+    }
+
+    public String getRespuesta(){
+        return respuesta;
     }
 }
